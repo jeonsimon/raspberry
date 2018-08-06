d586af08718ea94a7fbe989d5307d9cef2e97b17
jeonsimon <jeonsimon@gmail.com>
2018-08-06 23:48:21 +0900

    스위치를 눌러서 LED 켜는 작동을 해봄

---
+++ simple_led.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN)
+GPIO.setup(23, GPIO.OUT)
+
+try:
+    while True:
+        input_value = GPIO.input(18)
+
+        if input_value == False:
+            GPIO.output(23, True)
+            time.sleep(0.1)
+
+        else:
+            GPIO.output(23, False)
+            time.sleep(0.1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
